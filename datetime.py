90d6cfb1338c47a0f22b262fafd1b6e143a86513
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2024-07-25 21:22:48 +0200

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+print(datetime.now()) # 2024-07-25 21:20:13.970513
+print(type(datetime.now())) # <class 'datetime.datetime'>
+
+print(datetime.now().strftime("%Y")) # 2024
+print(datetime.now().strftime("%m")) # 07
+print(datetime.now().strftime("%d")) # 25
+print(datetime.now().strftime("%H")) # 21
+print(datetime.now().strftime("%M")) # 20
+
+print(datetime.now().strftime("Today is %d/%m/%Y.")) # Today is 25/07/2024.
+print(type(datetime.now().strftime("Today is %d/%m/%Y."))) # <class 'str'>
